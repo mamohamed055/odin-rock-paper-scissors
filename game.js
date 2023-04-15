09d8d0d4f7da48bedf20e1bb16133cfa2f065307
mohamed <95254152+mamohamed055@users.noreply.github.com>
2023-04-15 18:12:07 -0400

    Make it work using buttons and display result on paragraph instead of console

--- game.js
+++ game.js
@@ -4,63 +4,69 @@ function getComputerChoice() {
     return item;
 }
 
-function getPlayerChoice() {
-    let item = prompt("Please pick : rock, paper, or scissors");
-    return item.toLowerCase() ;
+function getPlayerChoice(playerSelection) {
+    return playerSelection;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+
+    computerSelection = getComputerChoice();
 
     if (playerSelection == "rock"){
+
         if (computerSelection == "rock")
         {
-            return "Draw!.";
+            document.getElementById("result").innerHTML = "Draw!.";
         }
         else if (computerSelection == "paper")
         {
-            return "Computer Wins!. Paper beats rock.";
+            document.getElementById("result").innerHTML = "Computer Wins!. Paper beats rock.";
         }
         else {
-            return "You win!. Rock beats scissors.";
+            document.getElementById("result").innerHTML = "You win!. Rock beats scissors.";
         }
+
     }
 
     else if (playerSelection == "paper") {
+
         if (computerSelection == "rock")
         {
-            return "You win!. Paper beats rock!.";
+            document.getElementById("result").innerHTML = "You win!. Paper beats rock!.";
         }
         else if (computerSelection == "paper")
         {
-            return "Draw!.";
+            document.getElementById("result").innerHTML = "Draw!.";
         }
         else {
-            return "Computer wins!. Scissors beat paper!.";
+            document.getElementById("result").innerHTML = "Computer wins!. Scissors beat paper!.";
         }
-    }
-    else if (playerSelection == "scissors") {
-        if (computerSelection == "rock")
-        {
-            return "Computer wins!. Rock beats scissors.";
-        }
-        else if (computerSelection == "paper")
-        {
-            return "You win!. Scissors beat paper!.";
-        }
-        else {
-            return "Draw!.";
-        }
+
     }
     else {
-        return "Please enter a valid selection!.";
+
+        if (computerSelection == "rock")
+        {
+            document.getElementById("result").innerHTML = "Computer wins!. Rock beats scissors.";
+        }
+        else if (computerSelection == "paper")
+        {
+            document.getElementById("result").innerHTML = "You win!. Scissors beat paper!.";
+        }
+        else {
+            document.getElementById("result").innerHTML = "Draw!.";
+        }
+
     }
 }
 
-function game() {
-    for (let i=0; i<5; i++){
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-        console.log(i);
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
+
+
+// function game() {
+//     for (let i=0; i<5; i++){
+//         let playerSelection = getPlayerChoice();
+//         let computerSelection = getComputerChoice();
+//         console.log(i);
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+// }
